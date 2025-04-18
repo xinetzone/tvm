440640f9597a3f3a08291c83c50260d49626a7ac
liuxinwei <liuxinwei@xmsilicon.cn>
2025-04-18 18:56:57 +0800

    重置文档

---
+++ xinetzone/old/vta/tutorials/frontend/set_env.py
@@ -0,0 +1,14 @@
+# 加载自定义库
+import sys
+from pathlib import Path
+from importlib import import_module
+
+TVM_ROOT = Path(__file__).absolute().parents[5]
+MOD_PATH = str(TVM_ROOT/'xinetzone/src')
+# print(MOD_PATH)
+
+if MOD_PATH not in sys.path:
+    sys.path.extend([MOD_PATH])
+
+tvmx = import_module('tvmx')
+tvmx.set_tvm(TVM_ROOT)
